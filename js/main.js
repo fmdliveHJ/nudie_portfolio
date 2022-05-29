069a1b224d93abe6d0b0e83ec8ace92e272fe39e
hyeonjin <gdalove@naver.com>
2022-05-30 01:00:54 +0900

    mobile 메뉴 1차 수정

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
+const btnCall = document.querySelector(".btnCall");
+const close = document.querySelector(".close");
+const menuMo = document.querySelector('.menuMo');
+btnCall.onclick = (e) => {
+  e.preventDefault();
+  menuMo.classList.add('on');
+}
+close.onclick = (e) => {
+  e.preventDefault();
+  menuMo.classList.remove('on');
 
+}
 
 
 const main = document.querySelectorAll('main');
